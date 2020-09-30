4b06600aac08842d4e36b19bb6e336a8dbff0564
Paul Okello <paulokello273@gmail.com>
2020-09-30 11:00:31 +0300

    setup pusher

--- server/server.js
+++ server/server.js
@@ -2,10 +2,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import Messages from "./dbMessages.js";
-
+import Pusher from "pusher";
 // app config
 const app = express();
 const port = process.env.PORT || 9000;
+
+const pusher = new Pusher({
+  appId: "1082342",
+  key: "c849a85ddc8a40a66142",
+  secret: "cea3ababb0437b59377f",
+  cluster: "ap2",
+  encrypted: true,
+});
 
 // middleware
 app.use(express.json());
@@ -19,6 +27,27 @@ mongoose.connect(connectionUrl, {
   useUnifiedTopology: true,
 });
 
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("DB connected");
+
+  const msgCollection = db.collection("messagecontents");
+  const changeStream = msgCollection.watch();
+
+  changeStream.on("change", (change) => {
+    console.log(change);
+
+    if (change.operationType === "insert") {
+      const messageDetails = change.fullDocument;
+      pusher.trigger("messages", "inserted", {
+        name: messageDetails.user,
+        message: messageDetails.message,
+      });
+    } else {
+      console.log("Error triggering Pusher");
+    }
+  });
+});
 // ???
 // api routes
 app.get("/", (req, res) => res.status(200).send("Hello Whatsapp"));
@@ -28,7 +57,7 @@ app.get("/messages/sync", (req, res) => {
     if (err) {
       res.status(500).send(err);
     } else {
-      res.status(200).send(data);
+      res.status(200).send(data); //Downloading data
     }
   });
 });
@@ -40,7 +69,7 @@ app.post("/messages/new", (req, res) => {
     if (err) {
       res.status(500).send(err);
     } else {
-      res.status(201).send(data);
+      res.status(201).send(data); // Creating data
     }
   });
 });
