23172ed76f1e2e7db0a7e80c33d43dff5bd292c2
Paul Okello <paulokello273@gmail.com>
2020-09-30 11:29:54 +0300

    Retrieve messages from backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
 import Pusher from "pusher-js";
+import axios from "./axios";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => {
+      console.log(response.data);
+      setMessages(response.data);
+    });
+  }, []);
   useEffect(() => {
     const pusher = new Pusher("c849a85ddc8a40a66142", {
       cluster: "ap2",
