0a04f9719929859e2bb29ca86f55f2a179502bee
Paul Okello <paulokello273@gmail.com>
2020-09-30 10:12:01 +0300

    MongoDb setup

--- server/server.js
+++ server/server.js
@@ -1,22 +1,39 @@
 // importing all modules
 import express from "express";
 import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
 
 // app config
 const app = express();
 const port = process.env.PORT || 9000;
 
 // middleware
+app.use(express.json());
 // Database config
-const connection_url =
-  "mongodb+srv://admin:50279909@Lopoz@cluster0.zidpr.mongodb.net/whatsapp-clone?retryWrites=true&w=majority";
-mongoose.connect(connection_url, {
+const connectionUrl =
+  "mongodb+srv://admin:hzgzH7Sxz5o5zESv@cluster0.zidpr.mongodb.net/whatsappDb?retryWrites=true&w=majority";
+
+mongoose.connect(connectionUrl, {
   useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 // ???
 // api routes
 app.get("/", (req, res) => res.status(200).send("Hello Whatsapp"));
+
+app.post("/messages/new", (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
 // listener
 app.listen(port, () => console.log(`Listenning on localhost: ${port}`));
